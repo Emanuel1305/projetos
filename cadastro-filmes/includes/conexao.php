9df2addc8ebb64b8aeb94557111f01d312b32987
emanuel-fagundes <emanuelfafa1313@gmail.com>
2024-12-02 23:20:46 -0500

    alt net

--- cadastro-filmes/includes/conexao.php
+++ cadastro-filmes/includes/conexao.php
@@ -1,8 +1,8 @@
 <?php 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'cadastros';
+$host = 'mysql';
+$user = 'emanuel-fagundes';
+$pass = '2022103030021';
+$db = 'emanuel-fagundes_banco';
 
 $conexao = mysqli_connect($host,$user,$pass,$db);
 ?>
